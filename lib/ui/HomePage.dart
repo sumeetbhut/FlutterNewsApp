1ceffc2c9bd7b116e9845d9920ca4692e8f641c8
Sumeet <sumeetbhut9@gmail.com>
2020-03-17 22:21:36 +0530

    Change app icon

--- lib/ui/HomePage.dart
+++ lib/ui/HomePage.dart
@@ -25,19 +25,12 @@ class _HomePageState extends State<HomePage> {
     loadingView();
     try {
       Response response = await Dio().get(
-          "http://newsapi.org/v2/top-headlines?sources=google-news&apiKey=c06c776c356f4cfd9eef7fdb4e3c26ac");
+          "http://newsapi.org/v2/top-headlines?sources=google-news&apiKey=YOUR API KEY");
       print(response);
       final newsModel = newsModelFromJson(response.toString());
 
-      print("Api Call :: > ${newsModel.toJson()['articles']}");
-
       var list = newsModel.toJson()['articles'] as List;
-      print(list.length);
-      print(list.runtimeType); //returns List<dynamic>
       List<News> newsList = list.map((i) => News.fromJson(i)).toList();
-      print(newsList.length);
-      var list2 = newsList[0].title;
-      Source list1 = newsList[0].source;
       List<NewsInsert> newsInsertTemp = [];
       newsInsertTemp.clear();
       for (var i = 0; i < newsList.length; i++) {
@@ -49,17 +42,13 @@ class _HomePageState extends State<HomePage> {
             newsList[i].urlToImage);
         newsInsertTemp.add(insert);
       }
-
       newsInsert.clear();
       setState(() {
         newsInsert.addAll(newsInsertTemp);
       });
-      print("Api phone x :: > ${list1.name}"); //returns List<dynamic>
-      print("Api phone list2 x :: > ${list2}"); //returns List<dynamic>
     } catch (e) {
       print(e);
     }
-    print("Api Callx :: > ${newsInsert[0].title}");
     return 10;
   }
 
